1ab91465f6d54b61f4e63d056e08af8ee5a2191a
sabbir322 <sabbirhosan370@gmail.com>
2022-04-21 13:04:17 +0600

    steps added

---
+++ __steps.js
@@ -0,0 +1,39 @@
+
+//  * -----------------------
+//  * BASIC SETUP
+//  * 1. Create a folder (manually or mkdiv)
+//  * 2. Open comand line to that folder
+//  * 3. npm init
+//  * 4. npm install express
+//  * 5. create index.js
+//  * ------------------------
+//  * For Express node server
+//  * ------------------------
+//  * 1. requir express
+//  * 2. create app variable
+//  * 3. declear port
+//  * 4. set app.get('/')
+//  * 5. listen to port 
+//  * 6. node index.js
+//  * 7. check your browser for that port
+//  * -------------------------
+//  * get
+//  * -------------------------
+//  * 1. corse meddleware
+//  * -------------------------
+//  * POST 
+//  * -------------------------
+//  * fetch("http://localhost:5000/user", {
+//  *   method: "POST",
+//  *   headers: {
+//  *     "content-Type": "application/json",
+//  *   },
+//  *   body: JSON.stringify(user),
+//  * })
+//  *   .then((res) => res.json())
+//  *   .then((data) => {
+//  *     const newUsers = [...users, data];
+//  *     setUsers(newUsers)
+//  *     console.log(data);
+//  *   });
+
